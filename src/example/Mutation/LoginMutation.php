17cea3d57fb1b335d1f3b4a5a0bece8a9ac15e99
Mikk Mihkel Nurges <mikk_nurges@hotmail.com>
2016-09-21 18:35:00 +0300

    Updated examples

---
+++ src/example/Mutation/LoginMutation.php
@@ -0,0 +1,49 @@
+<?php
+
+use GraphQL\GraphQL;
+use Rebing\GraphQL\Support\Mutation;
+use GraphQL\Type\Definition\Type;
+use Rebing\Services\Auth\UserLoginService; // not included in this project
+
+class LoginMutation extends Mutation {
+
+    protected $attributes = [
+        'name'          => 'Login',
+        'description'   => 'Log the user in by email',
+    ];
+
+    public function type()
+    {
+        return GraphQL::type('user');
+    }
+
+    public function args()
+    {
+        return [
+            'email' => [
+                'name'  => 'email',
+                'type'  => Type::nonNull(Type::string()),
+                'rules' => ['required', 'email'],
+            ],
+            'password' => [
+                'name'  => 'password',
+                'type'  => Type::nonNull(Type::string()),
+                'rules' => ['required', 'string'],
+            ],
+            'remember_me' => [
+                'name'  => 'remember_me',
+                'type'  => Type::boolean(),
+                'rules' => ['boolean'],
+            ],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        $loginService = new UserLoginService();
+        $user = $loginService->doLogin($args['email'], $args['password'], array_get($args, 'remember_me'));
+
+        return $user;
+    }
+
+}
