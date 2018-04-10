c4d3f151b34350c13af738c64cc7a42a881865b6
Yannick Yeboue <swinyx@users.noreply.github.com>
2018-04-10 09:17:27 -0400

    Update Mutation.php

--- src/Rebing/GraphQL/Support/Mutation.php
+++ src/Rebing/GraphQL/Support/Mutation.php
@@ -4,33 +4,4 @@ namespace Rebing\GraphQL\Support;
 
 class Mutation extends Field {
 
-    protected function rules(array $args = [])
-    {
-        return [];
-    }
-
-    public function getRules()
-    {
-        $arguments = func_get_args();
-
-        $rules = call_user_func_array([$this, 'rules'], $arguments);
-        $argsRules = [];
-        foreach($this->args() as $name => $arg)
-        {
-            if(isset($arg['rules']))
-            {
-                if(is_callable($arg['rules']))
-                {
-                    $argsRules[$name] = call_user_func_array($arg['rules'], $arguments);
-                }
-                else
-                {
-                    $argsRules[$name] = $arg['rules'];
-                }
-            }
-        }
-
-        return array_merge($argsRules, $rules);
-    }
-
 }
