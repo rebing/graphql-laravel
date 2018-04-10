225aa420acabebf57194a87eed7b77d704015b2b
Yannick Yeboue <swinyx@users.noreply.github.com>
2018-04-10 09:16:25 -0400

    Update Field.php

--- src/Rebing/GraphQL/Support/Field.php
+++ src/Rebing/GraphQL/Support/Field.php
@@ -33,6 +33,35 @@ class Field extends Fluent {
         return [];
     }
 
+    protected function rules(array $args = [])
+    {
+        return [];
+    }
+
+    public function getRules()
+    {
+        $arguments = func_get_args();
+
+        $rules = call_user_func_array([$this, 'rules'], $arguments);
+        $argsRules = [];
+        foreach($this->args() as $name => $arg)
+        {
+            if(isset($arg['rules']))
+            {
+                if(is_callable($arg['rules']))
+                {
+                    $argsRules[$name] = call_user_func_array($arg['rules'], $arguments);
+                }
+                else
+                {
+                    $argsRules[$name] = $arg['rules'];
+                }
+            }
+        }
+
+        return array_merge($argsRules, $rules);
+    }
+
     protected function getResolver()
     {
         if(!method_exists($this, 'resolve'))
