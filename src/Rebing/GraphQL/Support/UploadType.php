0ad2c030e3bd3dbd8d88d6381d7ce53d43c7058c
Dušan Plavák <dusan.plavak@gmail.com>
2018-05-21 13:29:46 +0200

    Added ability to upload files.

---
+++ src/Rebing/GraphQL/Support/UploadType.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rebing\GraphQL\Support;
+
+use GraphQL\Error\Error;
+use GraphQL\Error\InvariantViolation;
+use GraphQL\Type\Definition\ScalarType;
+use GraphQL\Utils\Utils;
+use \Illuminate\Http\UploadedFile;
+
+class UploadType extends ScalarType
+{
+    /**
+     * @var string
+     */
+    public $name = 'Upload';
+
+    /**
+     * @var string
+     */
+    public $description =
+        'The `Upload` special type represents a file to be uploaded in the same HTTP request as specified by
+ [graphql-multipart-request-spec](https://github.com/jaydenseric/graphql-multipart-request-spec).';
+
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        throw new InvariantViolation('`Upload` cannot be serialized');
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        if (!$value instanceof UploadedFile) {
+            throw new \UnexpectedValueException('Could not get uploaded file, be sure to conform to GraphQL multipart request specification. Instead got: ' . Utils::printSafe($value));
+        }
+
+        return $value;
+    }
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input
+     *
+     * @param \GraphQL\Language\AST\Node $valueNode
+     *
+     * @return mixed
+     */
+    public function parseLiteral($valueNode)
+    {
+        throw new Error('`Upload` cannot be hardcoded in query, be sure to conform to GraphQL multipart request specification. Instead got: ' . $valueNode->kind, [$valueNode]);
+    }
+}
