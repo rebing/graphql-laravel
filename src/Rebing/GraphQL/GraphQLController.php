634eb51309d7819786f06d56019d82fd33c40b5f
Mikk Mihkel Nurges <mikk_nurges@hotmail.com>
2019-05-21 12:53:16 +0000

    Apply fixes from StyleCI

--- src/Rebing/GraphQL/GraphQLController.php
+++ src/Rebing/GraphQL/GraphQLController.php
@@ -2,9 +2,9 @@
 
 namespace Rebing\GraphQL;
 
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Arr;
 
 class GraphQLController extends Controller
 {
