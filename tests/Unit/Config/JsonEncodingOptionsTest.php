2ec267bdefef3f4c9200c3d2487dcddd96f8be7f
Markus Fischer <markus@fischer.name>
2019-05-31 22:48:53 +0200

    tests: add for new config options headers and json_encoding_options

---
+++ tests/Unit/Config/JsonEncodingOptionsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rebing\GraphQL\Tests\Unit\Config;
+
+use Rebing\GraphQL\Tests\TestCase;
+use Rebing\GraphQL\Tests\Support\Objects\ExampleType;
+use Rebing\GraphQL\Tests\Support\Objects\ExamplesQuery;
+
+class JsonEncodingOptionsTest extends TestCase
+{
+    public function testCustomHeaders(): void
+    {
+        $response = $this->call('GET', '/graphql', [
+            'query' => $this->queries['examples'],
+        ]);
+
+        $json = <<<'JSON'
+{
+    "data": {
+        "examples": [
+            {
+                "test": "Example 1"
+            },
+            {
+                "test": "Example 2"
+            },
+            {
+                "test": "Example 3"
+            }
+        ]
+    }
+}
+JSON;
+        $this->assertSame($json, $response->content());
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('graphql', [
+            'json_encoding_options' => JSON_PRETTY_PRINT,
+
+            'schemas' => [
+                'default' => [
+                    'query' => [
+                        'examples' => ExamplesQuery::class,
+                    ],
+                ],
+            ],
+
+            'types' => [
+                'Example' => ExampleType::class,
+            ],
+        ]);
+    }
+}
