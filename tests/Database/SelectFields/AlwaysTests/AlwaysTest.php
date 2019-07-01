a05840ffd361ec1572b08863fc11617e112d771c
Markus Fischer <markus@fischer.name>
2019-07-01 23:30:07 +0200

    tests: use recent alias support for relations to improve always test coverage

--- tests/Database/SelectFields/AlwaysTests/AlwaysTest.php
+++ tests/Database/SelectFields/AlwaysTests/AlwaysTest.php
@@ -33,7 +33,7 @@ class AlwaysTest extends TestCaseDatabase
   alwaysQuery {
     body
     title
-    comments {
+    comments_always_single_field {
       id
     }
   }
@@ -56,7 +56,172 @@ SQL
                     [
                         'body' => 'post body',
                         'title' => 'post title',
-                        'comments' => [
+                        'comments_always_single_field' => [
+                            [
+                                'id' => (string) $comment->id,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testAlwaysSingleMultipleFieldInString(): void
+    {
+        /** @var Post $post */
+        $post = factory(Post::class)
+            ->create([
+                'body' => 'post body',
+                'title' => 'post title',
+            ]);
+        $comment = factory(Comment::class)
+            ->create([
+                'body' => 'comment body',
+                'post_id' => $post->id,
+                'title' => 'comment title',
+            ]);
+
+        $query = <<<'GRAQPHQL'
+{
+  alwaysQuery {
+    body
+    title
+    comments_always_multiple_fields_in_string {
+      id
+    }
+  }
+}
+GRAQPHQL;
+
+        $this->sqlCounterReset();
+
+        $result = $this->graphql($query);
+
+        $this->assertSqlQueries(<<<'SQL'
+select "posts"."body", "posts"."title", "posts"."id" from "posts";
+select "comments"."id", "comments"."post_id", "comments"."body", "comments"."title" from "comments" where "comments"."post_id" in (?) order by "comments"."id" asc;
+SQL
+        );
+
+        $expectedResult = [
+            'data' => [
+                'alwaysQuery' => [
+                    [
+                        'body' => 'post body',
+                        'title' => 'post title',
+                        'comments_always_multiple_fields_in_string' => [
+                            [
+                                'id' => (string) $comment->id,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testAlwaysSingleMultipleFieldInArray(): void
+    {
+        /** @var Post $post */
+        $post = factory(Post::class)
+            ->create([
+                'body' => 'post body',
+                'title' => 'post title',
+            ]);
+        $comment = factory(Comment::class)
+            ->create([
+                'body' => 'comment body',
+                'post_id' => $post->id,
+                'title' => 'comment title',
+            ]);
+
+        $query = <<<'GRAQPHQL'
+{
+  alwaysQuery {
+    body
+    title
+    comments_always_multiple_fields_in_array {
+      id
+    }
+  }
+}
+GRAQPHQL;
+
+        $this->sqlCounterReset();
+
+        $result = $this->graphql($query);
+
+        $this->assertSqlQueries(<<<'SQL'
+select "posts"."body", "posts"."title", "posts"."id" from "posts";
+select "comments"."id", "comments"."post_id", "comments"."body", "comments"."title" from "comments" where "comments"."post_id" in (?) order by "comments"."id" asc;
+SQL
+        );
+
+        $expectedResult = [
+            'data' => [
+                'alwaysQuery' => [
+                    [
+                        'body' => 'post body',
+                        'title' => 'post title',
+                        'comments_always_multiple_fields_in_array' => [
+                            [
+                                'id' => (string) $comment->id,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testAlwaysSameFieldTwice(): void
+    {
+        /** @var Post $post */
+        $post = factory(Post::class)
+            ->create([
+                'body' => 'post body',
+                'title' => 'post title',
+            ]);
+        $comment = factory(Comment::class)
+            ->create([
+                'body' => 'comment body',
+                'post_id' => $post->id,
+                'title' => 'comment title',
+            ]);
+
+        $query = <<<'GRAQPHQL'
+{
+  alwaysQuery {
+    body
+    title
+    comments_always_same_field_twice {
+      id
+    }
+  }
+}
+GRAQPHQL;
+
+        $this->sqlCounterReset();
+
+        $result = $this->graphql($query);
+
+        $this->assertSqlQueries(<<<'SQL'
+select "posts"."body", "posts"."title", "posts"."id" from "posts";
+select "comments"."id", "comments"."post_id", "comments"."body" from "comments" where "comments"."post_id" in (?) order by "comments"."id" asc;
+SQL
+        );
+
+        $expectedResult = [
+            'data' => [
+                'alwaysQuery' => [
+                    [
+                        'body' => 'post body',
+                        'title' => 'post title',
+                        'comments_always_same_field_twice' => [
                             [
                                 'id' => (string) $comment->id,
                             ],
--- tests/Database/SelectFields/AlwaysTests/PostType.php
+++ tests/Database/SelectFields/AlwaysTests/PostType.php
@@ -22,9 +22,25 @@ class PostType extends GraphQLType
             'body' => [
                 'type' => Type::string(),
             ],
-            'comments' => [
+            'comments_always_single_field' => [
                 'type' => Type::nonNull(Type::listOf(Type::nonNull(GraphQL::type('Comment')))),
+                'alias' => 'comments',
                 'always' => 'body',
+            ],
+            'comments_always_multiple_fields_in_string' => [
+                'type' => Type::nonNull(Type::listOf(Type::nonNull(GraphQL::type('Comment')))),
+                'alias' => 'comments',
+                'always' => 'body,title',
+            ],
+            'comments_always_multiple_fields_in_array' => [
+                'type' => Type::nonNull(Type::listOf(Type::nonNull(GraphQL::type('Comment')))),
+                'alias' => 'comments',
+                'always' => ['body', 'title'],
+            ],
+            'comments_always_same_field_twice' => [
+                'type' => Type::nonNull(Type::listOf(Type::nonNull(GraphQL::type('Comment')))),
+                'alias' => 'comments',
+                'always' => ['body', 'body'],
             ],
             'id' => [
                 'type' => Type::nonNull(Type::ID()),
