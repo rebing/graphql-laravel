879f7633ac675088a1cbf230f088b1b542edcfd3
Markus Fischer <markus@fischer.name>
2019-07-07 22:07:47 +0200

    input make: remove `Type` from GraphQL name if present

--- src/Console/InterfaceMakeCommand.php
+++ src/Console/InterfaceMakeCommand.php
@@ -21,4 +21,23 @@ class InterfaceMakeCommand extends GeneratorCommand
     {
         return $rootNamespace.'\GraphQL\Interfaces';
     }
+
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        return $this->replaceGraphqlName($stub);
+    }
+
+    protected function replaceGraphqlName(string $stub): string
+    {
+        $graphqlName = $this->getNameInput();
+        $graphqlName = preg_replace('/Type$/', '', $graphqlName);
+
+        return str_replace(
+            'DummyGraphqlName',
+            $graphqlName,
+            $stub
+        );
+    }
 }
--- tests/Unit/Console/InterfaceMakeCommandTest.php
+++ tests/Unit/Console/InterfaceMakeCommandTest.php
@@ -25,13 +25,127 @@ class InterfaceMakeCommandTest extends TestCase
             ->method('put')
             ->with(
                 $this->callback(function (string $path): bool {
+                    $this->assertRegExp('|laravel[/\\\\]app/GraphQL/Interfaces/Example.php|', $path);
+
+                    return true;
+                }),
+                $this->callback(function (string $contents): bool {
+                    $this->assertRegExp('/class Example extends InterfaceType/', $contents);
+                    $this->assertRegExp("/'name' => 'Example',/", $contents);
+
+                    return true;
+                })
+            );
+        $this->instance(Filesystem::class, $filesystemMock);
+
+        $command = $this->app->make(InterfaceMakeCommand::class);
+
+        $tester = $this->runCommand($command, [
+            'name' => 'Example',
+        ]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertRegExp('/Interface created successfully/', $tester->getDisplay());
+    }
+
+    public function testEndsWithType(): void
+    {
+        $filesystemMock = $this
+            ->getMockBuilder(Filesystem::class)
+            ->setMethods([
+                'isDirectory',
+                'makeDirectory',
+                'put',
+            ])
+            ->getMock();
+        $filesystemMock
+            ->expects($this->once())
+            ->method('put')
+            ->with(
+                $this->callback(function (string $path): bool {
+                    $this->assertRegExp('|laravel[/\\\\]app/GraphQL/Interfaces/ExampleType.php|', $path);
+
+                    return true;
+                }),
+                $this->callback(function (string $contents): bool {
+                    $this->assertRegExp('/class ExampleType extends InterfaceType/', $contents);
+                    $this->assertRegExp("/'name' => 'Example',/", $contents);
+
+                    return true;
+                })
+            );
+        $this->instance(Filesystem::class, $filesystemMock);
+
+        $command = $this->app->make(InterfaceMakeCommand::class);
+
+        $tester = $this->runCommand($command, [
+            'name' => 'ExampleType',
+        ]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertRegExp('/Interface created successfully/', $tester->getDisplay());
+    }
+
+    public function testEndsWithInterface(): void
+    {
+        $filesystemMock = $this
+            ->getMockBuilder(Filesystem::class)
+            ->setMethods([
+                'isDirectory',
+                'makeDirectory',
+                'put',
+            ])
+            ->getMock();
+        $filesystemMock
+            ->expects($this->once())
+            ->method('put')
+            ->with(
+                $this->callback(function (string $path): bool {
+                    $this->assertRegExp('|laravel[/\\\\]app/GraphQL/Interfaces/ExampleInterface.php|', $path);
+
+                    return true;
+                }),
+                $this->callback(function (string $contents): bool {
+                    $this->assertRegExp('/class ExampleInterface extends InterfaceType/', $contents);
+                    $this->assertRegExp("/'name' => 'ExampleInterface',/", $contents);
+
+                    return true;
+                })
+            );
+        $this->instance(Filesystem::class, $filesystemMock);
+
+        $command = $this->app->make(InterfaceMakeCommand::class);
+
+        $tester = $this->runCommand($command, [
+            'name' => 'ExampleInterface',
+        ]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertRegExp('/Interface created successfully/', $tester->getDisplay());
+    }
+
+    public function testEndsWithInterfaceType(): void
+    {
+        $filesystemMock = $this
+            ->getMockBuilder(Filesystem::class)
+            ->setMethods([
+                'isDirectory',
+                'makeDirectory',
+                'put',
+            ])
+            ->getMock();
+        $filesystemMock
+            ->expects($this->once())
+            ->method('put')
+            ->with(
+                $this->callback(function (string $path): bool {
                     $this->assertRegExp('|laravel[/\\\\]app/GraphQL/Interfaces/ExampleInterfaceType.php|', $path);
 
                     return true;
                 }),
                 $this->callback(function (string $contents): bool {
                     $this->assertRegExp('/class ExampleInterfaceType extends InterfaceType/', $contents);
-                    $this->assertRegExp("/'name' => 'ExampleInterfaceType',/", $contents);
+                    $this->assertRegExp("/'name' => 'ExampleInterface',/", $contents);
 
                     return true;
                 })
