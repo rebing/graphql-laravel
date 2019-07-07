1ca7042ead729a3bb56c61ca67ce37515cbac2da
Markus Fischer <markus@fischer.name>
2019-07-07 08:08:30 +0200

    Tidy up make command generators
    - The comment vs. code ratio was very high and practically added nothing
    - Removed some custom replacers which are easily covered with the existing replacement

--- src/Console/InterfaceMakeCommand.php
+++ src/Console/InterfaceMakeCommand.php
@@ -8,78 +8,17 @@ use Illuminate\Console\GeneratorCommand;
 
 class InterfaceMakeCommand extends GeneratorCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'make:graphql:interface {name}';
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a new GraphQL interface class';
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
     protected $type = 'Interface';
 
-    /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
     protected function getStub()
     {
         return __DIR__.'/stubs/interface.stub';
     }
 
-    /**
-     * Get the default namespace for the class.
-     *
-     * @param string $rootNamespace
-     *
-     * @return string
-     */
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace.'\GraphQL\Interfaces';
-    }
-
-    /**
-     * Build the class with the given name.
-     *
-     * @param string $name
-     *
-     * @return string
-     */
-    protected function buildClass($name)
-    {
-        $stub = parent::buildClass($name);
-
-        return $this->replaceType($stub, $name);
-    }
-
-    /**
-     * Replace the namespace for the given stub.
-     *
-     * @param string $stub
-     * @param string $name
-     *
-     * @return string
-     */
-    protected function replaceType(string $stub, string $name): string
-    {
-        preg_match('/([^\\\]+)$/', $name, $matches);
-        $stub = str_replace(
-            'DummyInterfaceType',
-            $matches[1],
-            $stub
-        );
-
-        return $stub;
     }
 }
--- src/Console/MutationMakeCommand.php
+++ src/Console/MutationMakeCommand.php
@@ -8,54 +8,20 @@ use Illuminate\Console\GeneratorCommand;
 
 class MutationMakeCommand extends GeneratorCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'make:graphql:mutation {name}';
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a new GraphQL mutation class';
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
     protected $type = 'Mutation';
 
-    /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
     protected function getStub()
     {
         return __DIR__.'/stubs/mutation.stub';
     }
 
-    /**
-     * Get the default namespace for the class.
-     *
-     * @param string $rootNamespace
-     *
-     * @return string
-     */
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace.'\GraphQL\Mutations';
     }
 
-    /**
-     * Build the class with the given name.
-     *
-     * @param string $name
-     *
-     * @return string
-     */
     protected function buildClass($name)
     {
         $stub = parent::buildClass($name);
