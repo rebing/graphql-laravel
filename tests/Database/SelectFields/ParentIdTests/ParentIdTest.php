0de741bca33c5c3f6101fdca29d675248ba3182c
Argentum88 <argentimgipan@gmail.com>
2019-07-22 14:16:49 +0500

    disabled lazyload_types on pagination type

--- tests/Database/SelectFields/ParentIdTests/ParentIdTest.php
+++ tests/Database/SelectFields/ParentIdTests/ParentIdTest.php
@@ -108,6 +108,8 @@ SQL
     {
         parent::getEnvironmentSetUp($app);
 
+        $app['config']->set('graphql.lazyload_types', false);
+
         $app['config']->set('graphql.schemas.default', [
             'query' => [
                 ParentIdQuery::class,
