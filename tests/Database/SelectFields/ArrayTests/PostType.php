4b32e3eb07758a32489c3381f912d9ac37db8195
Argentum88 <argentimgipan@gmail.com>
2019-07-24 16:48:45 +0500

    added arrayFieldRetrieved test

---
+++ tests/Database/SelectFields/ArrayTests/PostType.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rebing\GraphQL\Tests\Database\SelectFields\ArrayTests;
+
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Tests\Support\Models\Post;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class PostType extends GraphQLType
+{
+    protected $attributes = [
+        'name' => 'Post',
+        'model' => Post::class,
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::ID()),
+            ],
+            'properties' => [
+                'type' => Type::listOf(GraphQL::type('Property')),
+                'selectable' => false,
+            ],
+        ];
+    }
+
+    protected function resolvePropertiesField($root, $args)
+    {
+        return $root->properties;
+    }
+}
