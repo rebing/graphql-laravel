66d32dbe880078b0d77a79907b0a5cf8d907f3eb
Markus Podar <markus@fischer.name>
2021-05-12 17:48:29 +0200

    Prefer using Auth Factory over Facade

--- src/Support/ExecutionMiddleware/AddAuthUserContextValueMiddleware.php
+++ src/Support/ExecutionMiddleware/AddAuthUserContextValueMiddleware.php
@@ -6,15 +6,23 @@ namespace Rebing\GraphQL\Support\ExecutionMiddleware;
 use Closure;
 use GraphQL\Executor\ExecutionResult;
 use GraphQL\Type\Schema;
-use Illuminate\Container\Container;
+use Illuminate\Contracts\Auth\Factory;
 use Rebing\GraphQL\Support\OperationParams;
 
 class AddAuthUserContextValueMiddleware extends AbstractExecutionMiddleware
 {
+    /** @var Factory */
+    private $auth;
+
+    public function __construct(Factory $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function handle(string $schemaName, Schema $schema, OperationParams $params, $rootValue, $contextValue, Closure $next): ExecutionResult
     {
         if (null === $contextValue) {
-            $contextValue = Container::getInstance()->make('auth')->user();
+            $contextValue = $this->auth->guard()->user();
         }
 
         return $next($schemaName, $schema, $params, $rootValue, $contextValue);
