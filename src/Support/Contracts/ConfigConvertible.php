a0aa54ec8c4a2fd8c6902a91a60278de4492b868
Markus Podar <markus@fischer.name>
2021-06-10 20:41:11 +0200

    Increase specificity of the expected value return by toConfig

--- src/Support/Contracts/ConfigConvertible.php
+++ src/Support/Contracts/ConfigConvertible.php
@@ -3,10 +3,18 @@
 declare(strict_types = 1);
 namespace Rebing\GraphQL\Support\Contracts;
 
+use Rebing\GraphQL\Support\ExecutionMiddleware\AbstractExecutionMiddleware;
+
 interface ConfigConvertible
 {
     /**
-     * @return array<array>
+     * @return array{
+     *                query:array<string,class-string>|array<class-string>,
+     *                mutation?:array<string,class-string>|array<class-string>,
+     *                types?:array<string,class-string>|array<class-string>,
+     *                middleware?:array<string|class-string>,
+     *                execution_middleware?:array<class-string<AbstractExecutionMiddleware>>
+     *                }
      */
     public function toConfig(): array;
 }
