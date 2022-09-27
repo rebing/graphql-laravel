da8b15361f22b19eaf6e830331f1b459afcb3bc7
Simon Verwaard <simon@bttr.nl>
2022-09-27 09:02:35 +0200

    test: add test for registration of directives via buildSchemaFromConfig

---
+++ tests/Support/Directives/ExampleDirective.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+namespace Rebing\GraphQL\Tests\Support\Directives;
+
+use GraphQL\Language\DirectiveLocation;
+use GraphQL\Type\Definition\Directive;
+use GraphQL\Type\Definition\FieldArgument;
+use GraphQL\Type\Definition\Type;
+
+class ExampleDirective extends Directive
+{
+    public function __construct()
+    {
+        parent::__construct([
+            'name' => 'exampleDirective',
+            'description' => 'This is an example directive',
+            'locations' => [
+                // See DirectiveLocation constants for all available locations
+                DirectiveLocation::QUERY,
+            ],
+            'args' => [
+                new FieldArgument([
+                    'name' => 'first',
+                    'description' => 'Description of this argument',
+                    'type' => Type::string(),
+                ]),
+            ],
+        ]);
+    }
+}
