b55992ed39f30dc9a3b66ee97f7200d60705e322
Colant Alan <19172637+alancolant@users.noreply.github.com>
2023-07-08 14:07:57 +0200

    add test for route parameters schema

---
+++ tests/Unit/EndpointParamsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rebing\GraphQL\Tests\Unit;
+
+use Illuminate\Contracts\Config\Repository;
+use Rebing\GraphQL\GraphQLController;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Tests\TestCase;
+
+class EndpointParamsTest extends TestCase
+{
+    public function testGetDefaultSchemaWithRouteParameter(): void
+    {
+        $response = $this->call('GET', '/graphql/arbitrary_param', [
+            'query' => $this->queries['examples'],
+        ]);
+
+        self::assertEquals(200, $response->getStatusCode());
+
+        $content = $response->getData(true);
+        self::assertArrayHasKey('data', $content);
+        self::assertEquals($content['data'], [
+            'examples' => $this->data,
+        ]);
+    }
+    public function testGetCustomSchemaWithRouteParameter(): void
+    {
+        $response = $this->call('GET', '/graphql/arbitrary_param/custom', [
+            'query' => $this->queries['examplesCustom'],
+        ]);
+        self::assertEquals(200, $response->getStatusCode());
+        $content = $response->getData(true);
+        self::assertArrayHasKey('data', $content);
+        self::assertEquals($content['data'], [
+            'examplesCustom' => $this->data,
+        ]);
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        parent::getEnvironmentSetUp($app);
+        $app['config']->set('graphql.route.prefix', 'graphql/{parameter}');
+    }
+
+}
