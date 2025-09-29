853bd2bf0875976878a5ee9bb9c9855e25e92735
Ali Sasani <a_S47_1@yahoo.com>
2025-09-30 00:45:54 +0330

    test(routes): add unit test for routes with schema attributes

---
+++ tests/Unit/RouteWithSchemaTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types = 1);
+namespace Rebing\GraphQL\Tests\Unit;
+
+use Illuminate\Routing\Route;
+use Illuminate\Support\Collection;
+use Rebing\GraphQL\Tests\Support\Objects\ExampleMiddleware;
+use Rebing\GraphQL\Tests\TestCase;
+
+class RouteWithSchemaTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app): void
+    {
+        // Laravel registers routes for local filesystems by default.
+        // However, for the purpose of this test, we don't want it to register any routes.
+        $app['config']->set('filesystems.disks.local.serve', false);
+
+        $app['config']->set('graphql', [
+            'route' => [
+                'prefix' => 'graphql_test',
+            ],
+
+            'schemas' => [
+                'default' => [
+                    'middleware' => [ExampleMiddleware::class],
+                ],
+                'with_schema_attributes' => [
+                    'middleware' => [ExampleMiddleware::class],
+                    'schema_attributes' => [
+                        'domain' => 'api.example.com',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testRoutesWithSchemaAttributes(): void
+    {
+        $expected = [
+            'graphql' => [
+                'methods' => [
+                    'GET',
+                    'POST',
+                    'HEAD',
+                ],
+                'uri' => 'graphql_test',
+                'middleware' => [
+                    ExampleMiddleware::class,
+                ],
+                'domain' => null,
+                'action_middleware' => [ExampleMiddleware::class],
+                'action_excluded_middleware' => [],
+            ],
+            'graphql.default' => [
+                'methods' => [
+                    'GET',
+                    'POST',
+                    'HEAD',
+                ],
+                'uri' => 'graphql_test/default',
+                'middleware' => [
+                    ExampleMiddleware::class,
+                ],
+                'domain' => null,
+                'action_middleware' => [ExampleMiddleware::class],
+                'action_excluded_middleware' => [],
+            ],
+            'graphql.with_schema_attributes' => [
+                'methods' => [
+                    'GET',
+                    'POST',
+                    'HEAD',
+                ],
+                'uri' => 'graphql_test/with_schema_attributes',
+                'middleware' => [
+                    ExampleMiddleware::class,
+                ],
+                'domain' => 'api.example.com',
+                'action_middleware' => [ExampleMiddleware::class],
+                'action_excluded_middleware' => [],
+            ],
+        ];
+
+        $actual = Collection::make(
+            app('router')->getRoutes()->getRoutesByName()
+        )->map(function (Route $route) {
+            $action = $route->getAction();
+            return [
+                'methods' => $route->methods(),
+                'uri' => $route->uri(),
+                'middleware' => $route->middleware(),
+                'domain' => method_exists($route, 'getDomain') ? $route->getDomain() : null,
+                'action_middleware' => $action['middleware'] ?? [],
+                'action_excluded_middleware' => $action['excluded_middleware'] ?? [],
+            ];
+        })->all();
+
+        self::assertEquals($expected, $actual);
+    }
+}
+
+
