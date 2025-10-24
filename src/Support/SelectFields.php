b73ec76a62db4f24a93128b748f7ab0e31609070
Ali Sasani <a_S47_1@yahoo.com>
2025-10-25 01:14:56 +0330

    [feat] change method WithMorph to constrain on union types

--- src/Support/SelectFields.php
+++ src/Support/SelectFields.php
@@ -588,7 +588,7 @@ class SelectFields
             }
 
             if (!empty($morphRelation)) {
-                $relation->morphWith($morphRelation);
+                $relation->constrain($morphRelation);
             }
         };
 
